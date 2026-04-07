353806c7c218f21ec32e13a72ea959dd283e9444
Akshat-tyagi <akshattyagi146@gmail.com>
2026-04-08 03:14:42 +0530

    initialised worker in index file

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import authroutes from "./routes/authroutes.js";
 import monitorRoutes from "./routes/monitor.routes.js";
+import monitorWorker from "./workers/monitor.worker.js";
 
 const app = express();
 
@@ -13,3 +14,5 @@ app.use("/api/login", authroutes);
 app.use("/api/monitors", monitorRoutes);
 
 app.listen(3000);
+
+console.log("Worker status:", monitorWorker ? "Active" : "Inactive");
