79d423b90cf8b2d420a5386baf1b528b9c4155b3
Akshat-tyagi <akshattyagi146@gmail.com>
2026-04-10 13:52:01 +0530

    Remove duplicate backend files from root

--- src/queue/monitor.queue.ts
+++
@@ -1,8 +0,0 @@
-import { Queue } from "bullmq";
-import redis from "../db/redis.js";
-
-export const monitorQueue = new Queue("monitors",{
-    connection:redis,
-});
-
-export default monitorQueue;
--- src/routes/authroutes.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import authcontrollers from "../controllers/authcontrollers.js";
-
-const router = express.Router();
-
-router.post("/signup",authcontrollers.signupuser);
-router.post("/signin",authcontrollers.signinuser);
-
-export default router;
